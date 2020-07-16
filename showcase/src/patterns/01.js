de4a7c0913d176fb1a327d46e91f0e53d3d35872
Leila <nb.leila10@gmail.com>
2020-07-16 18:08:13 -0500

    Animate scale of the clap

--- showcase/src/patterns/01.js
+++ showcase/src/patterns/01.js
@@ -1,4 +1,5 @@
 import React, { useState, Component } from "react";
+import mojs from "mo-js";
 import styles from "./index.css";
 
 const initialState = {
@@ -13,24 +14,41 @@ const initialState = {
 
 const withClapAnimation = (WrappedComponent) => {
   class withClapAnimation extends Component {
-    animate = () => {
-      console.log("animating");
+    animationTimeline = new mojs.Timeline();
+    state = {
+      animationTimeline: this.animationTimeline,
     };
+
+    componentDidMount() {
+      const scaleButton = new mojs.Html({
+        el: "#clap",
+        duration: 300,
+        scale: { 1.3: 1 },
+        easing: mojs.easing.ease.out,
+      });
+
+      const clap = document.getElementById("clap");
+      clap.style.transform = "scale(1,1)";
+
+      const newAnimationTimeline = this.animationTimeline.add([scaleButton]);
+      this.setState({ animationTimeline: newAnimationTimeline });
+    }
+
     render() {
-      return <WrappedComponent {...this.props} animate={this.animate} />;
+      return <WrappedComponent {...this.props} animationTimeline={this.state.animationTimeline} />;
     }
   }
 
   return withClapAnimation;
 };
 
-const MediumClap = ({ animate }) => {
+const MediumClap = ({ animationTimeline }) => {
   const MAXIMUM_CLAP = 50;
   const [clapState, setClapState] = useState(initialState);
   const { count, countTotal, isClicked } = clapState;
 
   const handleClapClick = () => {
-    animate();
+    animationTimeline.replay();
     setClapState((prevState) => ({
       isClicked: true,
       count: Math.min(prevState.count + 1, MAXIMUM_CLAP),
@@ -39,7 +57,7 @@ const MediumClap = ({ animate }) => {
   };
 
   return (
-    <button className={styles.clap} onClick={handleClapClick}>
+    <button id='clap' className={styles.clap} onClick={handleClapClick}>
       <ClapIcon isClicked={isClicked} />
       <ClapCount count={count} />
       <ClapTotal countTotal={countTotal} />
